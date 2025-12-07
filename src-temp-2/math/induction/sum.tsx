d1e3b530e98134c0f842bd9bf3566897d9bc8b89
Martin Geisse <martin.geisse@janitza.de>
2025-12-07 11:02:22 +0100

    ...

--- src-temp-2/math/induction/sum.tsx
+++ src-temp-2/math/induction/sum.tsx
@@ -10,38 +10,7 @@ import {randomElement} from "../../../src/framework/util/random/randomElement.ts
 // nothing is randomized in the instances, only the choice of instance is
 export const sumExerciseInstances: ExerciseInstance[] = [
     
-  natInductionExercise(
-      <>{mathSpan("1+2+3+...+n = #sum_{i=1}^ni = #frac{n(n+1)}2")}</>,
-      mathDiv("#sum_{i=1}^ni = #sum_{i=1}^1i = 1 = #frac{1(1+1)}2"),
-      <>{mathSpan("#sum_{i=1}^{n+1}i = #frac{(n+1)((n+1)+1)}2")}</>,
-      _detailLevel => <>
-        {mathDiv("#sum_{i=1}^{n+1}i")}
-        {mathDiv("= (n+1) + #sum_{i=1}^ni")}
-        <div>using the induction hypothesis:</div>
-        {mathDiv("= (n+1) + #frac{n(n+1)}2")}
-        {mathDiv("= #frac{2(n+1)}2 + #frac{n(n+1)}2")}
-        {mathDiv("= #frac{2(n+1) + n(n+1)}2")}
-        {mathDiv("= #frac{(n+1)(n+2)}2")}
-      </>
-  ),
 
-  natInductionExercise(
-      <>{mathSpan("1^2+2^2+3^2+...+n^2 = #sum_{i=1}^ni^2 = #frac{n(n+1)(2n+1)}6")}</>,
-      mathDiv("#sum_{i=1}^ni^2 = #sum_{i=1}^1i^2 = 1^2 = 1 = #frac{1#cdot 2#cdot 3}6 = #frac{n(n+1)(2n+1)}6"),
-      <>{mathSpan("#sum_{i=1}^{n+1}i^2 = #frac{(n+1)((n+1)+1)(2(n+1)+1)}6 = #frac{(n+1)(n+2)(2n+3)}6")}</>,
-      _detailLevel => <>
-        {mathDiv("#sum_{i=1}^{n+1}i^2")}
-        {mathDiv("= (n+1)^2 + #sum_{i=1}^ni^2")}
-        <div>using the induction hypothesis:</div>
-        {mathDiv("= (n+1)^2 + #frac{n(n+1)(2n+1)}6")}
-        {mathDiv("= #frac{6(n+1)^2}6 + #frac{n(n+1)(2n+1)}6")}
-        {mathDiv("= #frac{6(n+1)^2 + n(n+1)(2n+1)}6")}
-        {mathDiv("= #frac{(n+1)(6(n+1) + n(2n+1))}6")}
-        {mathDiv("= #frac{(n+1)(6n + 6 + 2n^2 + n))}6")}
-        {mathDiv("= #frac{(n+1)(2n^2 + 7n + 6))}6")}
-        {mathDiv("= #frac{(n+1)(n+2)(2n+3)}6")}
-      </>
-  ),
 
   natInductionExercise(
       <>{mathSpan("1^3+2^3+3^3+...+n^3 = #sum_{i=1}^ni^3 = #frac{n^2(n+1)^2}4")}</>,
@@ -82,20 +51,6 @@ export const sumExerciseInstances: ExerciseInstance[] = [
         {mathDiv("= #frac{(n+1)(n+2)}{30}(6n^3 + 27n^2 + 37n + 15)")}
         {mathDiv("= #frac{(n+1)(n+2)}{30}(2n + 3)(3n^2 + 9n + 5)")}
         <div style={{fontSize: "0.9em"}}>{mathDiv("= #frac{(n+1)((n+1)+1)(2(n+1)+1)(3(n+1)^2+3(n+1)-1)}{30}")}</div>
-      </>
-  ),
-
-  natInductionExercise(
-      <>{mathSpan("1+3+5+...+(2n-1) = #sum_{i=1}^n(2i-1) = n^2")}</>,
-      <>{mathDiv("#sum_{i=1}^n(2i-1) = #sum_{i=1}^1(2i-1) = 2#cdot 1 - 1 = 1 = 1^2")}</>,
-      mathSpan("#sum_{i=1}^{n+1}(2i-1) = (n+1)^2"),
-      _detailLevel => <>
-        {mathDiv("#sum_{i=1}^{n+1}(2i-1)")}
-        {mathDiv("= (2(n+1)-1) + #sum_{i=1}^n(2i-1)")}
-        {mathDiv("= (2n+1) + #sum_{i=1}^n(2i-1)")}
-        <div>using the induction hypothesis:</div>
-        {mathDiv("= 2n + 1 + n^2")}
-        {mathDiv("= (n+1)^2")}
       </>
   ),
 
--- src/content/math/calculus/induction/inductionSums.tsx
+++ src/content/math/calculus/induction/inductionSums.tsx
@@ -1,6 +1,49 @@
-import type {ExerciseNode} from "../../../../framework/content.tsx";
+import type {ContentItem, ExerciseNode} from "../../../../framework/content.tsx";
 import {mathDiv, mathSpan} from "../../../../framework/technical-components/Math/Math.tsx";
 import {isNatPlus} from "../../util/math-atoms.tsx";
+import type {ReactNode} from "react";
+import {mathDivOrCustom} from "../../util/math-util.tsx";
+
+interface ExtraOptions {
+  problemPrelude?: ReactNode | null | undefined;
+  solutionFooter?: ReactNode | null | undefined;
+}
+
+const defaultExtraOptions: ExtraOptions = {
+  problemPrelude: <></>,
+  solutionFooter: <></>,
+};
+
+function standardSumContentItem(
+    equationToProve: string | ReactNode,
+    baseCaseProof: string | ReactNode, // a string will be fed to mathDiv, a ReactNode will be used as-is
+    nextEquationToProve: string | ReactNode,
+    nextEquationProof: (string | ReactNode)[],
+    extraOptions?: ExtraOptions | null | undefined,
+): ContentItem {
+  const materializedExtraOptions: ExtraOptions = {...defaultExtraOptions, ...extraOptions};
+  const equationToProveDiv = mathDivOrCustom(equationToProve);
+  const nextEquationToProveDiv = mathDivOrCustom(nextEquationToProve);
+  return {
+    problem: <>
+      {materializedExtraOptions.problemPrelude}
+      <p>Use induction to show that</p>
+      {equationToProveDiv}
+      <p>for {isNatPlus("n")}.</p>
+    </>,
+    answer: <>
+      <p>Base case ({mathSpan("n = 1")}):</p>
+      {mathDivOrCustom(baseCaseProof)}
+      <p>Induction step: Assume that</p>
+      {equationToProveDiv}
+      <p>(induction hypothesis). Then show that</p>
+      {nextEquationToProveDiv}
+      <p>Proof:</p>
+      {nextEquationProof.map(mathDivOrCustom)}
+      {defaultExtraOptions.solutionFooter}
+    </>,
+  };
+}
 
 export const inductionSums: ExerciseNode = {
   id: "sums",
@@ -74,5 +117,25 @@ export const inductionSums: ExerciseNode = {
         {mathDiv("= (n+1)^2")}
       </>,
     },
+    standardSumContentItem(
+        "#sum_{i=1}^ni^2 = #frac{n(n+1)(2n+1)}6",
+        "#sum_{i=1}^ni^2 = #sum_{i=1}^1i^2 = 1^2 = 1 = #frac{1#cdot 2#cdot 3}6 = #frac{n(n+1)(2n+1)}6",
+        <>
+          {mathDiv("#sum_{i=1}^{n+1}i^2 = #frac{(n+1)((n+1)+1)(2(n+1)+1)}6")}
+          {mathDiv("= #frac{(n+1)(n+2)(2n+3)}6")}
+        </>,
+        [
+          "#sum_{i=1}^{n+1}i^2",
+          "= (n+1)^2 + #sum_{i=1}^ni^2",
+          <div>using the induction hypothesis:</div>,
+          "= (n+1)^2 + #frac{n(n+1)(2n+1)}6",
+          "= #frac{6(n+1)^2}6 + #frac{n(n+1)(2n+1)}6",
+          "= #frac{6(n+1)^2 + n(n+1)(2n+1)}6",
+          "= #frac{(n+1)(6(n+1) + n(2n+1))}6",
+          "= #frac{(n+1)(6n + 6 + 2n^2 + n))}6",
+          "= #frac{(n+1)(2n^2 + 7n + 6))}6",
+          "= #frac{(n+1)(n+2)(2n+3)}6",
+        ],
+    ),
   ],
 };

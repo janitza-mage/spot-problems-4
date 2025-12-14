ce1e5e6e39ed922722907533cf5af56302b7d755
Martin Geisse <martin.geisse@janitza.de>
2025-12-14 17:23:09 +0100

    ...

--- src/content/math/calculus/field/fieldExponentiation.tsx
+++ src/content/math/calculus/field/fieldExponentiation.tsx
@@ -21,48 +21,86 @@ export const fieldExponentiation: ExerciseNode = {
         <p>
           {letField} Let {mathSpan("a #in F")}, {isNat0("n")}. Use induction to prove that
         </p>
-        {mathDiv("x^{-n} = (x^n)^{-1}")}
+        {mathDiv("a^{-n} = (a^n)^{-1}")}
         <p>
           Note that by definition,
         </p>
-        {mathDiv("x^{-n} = (x^{-1})^n")}
+        {mathDiv("a^{-n} = (a^{-1})^n")}
         <p>
           You can use the fact that
         </p>
-        {mathDiv("a^{-1}b^{-1}=(ab)^{-1}")}
+        {mathDiv("x^{-1}y^{-1}=(xy)^{-1}")}
       </>,
       answer: <>
         <p>
           Base case ({mathSpan("n=0")}). From the axioms,
         </p>
-        {mathDiv("x^{-n} = x^{-0} = (x^{-1})^0 = 1 = 1^{-1} = (x^0)^{-1} = (x^n)^{-1}")}
+        {mathDiv("a^{-n} = a^{-0} = (a^{-1})^0 = 1 = 1^{-1} = (a^0)^{-1} = (a^n)^{-1}")}
         <p>
           Indcution step: Assume that
         </p>
-        {mathDiv("x^{-n} = (x^n)^{-1}")}
+        {mathDiv("a^{-n} = (a^n)^{-1}")}
         <p>
           Then, show that 
         </p>
-        {mathDiv("x^{-(n+1)} = (x^{n+1})^{-1}")}
+        {mathDiv("a^{-(n+1)} = (a^{n+1})^{-1}")}
         <p>
           Proof:
         </p>
-        {mathDiv("x^{-(n+1)}")}
-        {mathDiv("(x^{-1})^{n+1}")}
-        {mathDiv("(x^{-1})^n(x^{-1})")}
-        {mathDiv("x^{-n}x^{-1}")}
+        {mathDiv("a^{-(n+1)}")}
+        {mathDiv("(a^{-1})^{n+1}")}
+        {mathDiv("(a^{-1})^n(a^{-1})")}
+        {mathDiv("a^{-n}a^{-1}")}
         <p>Using the induction hypothesis:</p>
-        {mathDiv("(x^n)^{-1}x^{-1}")}
-        {mathDiv("(x^nx)^{-1}")}
-        {mathDiv("(x^{n+1})^{-1}")}
+        {mathDiv("(a^n)^{-1}a^{-1}")}
+        {mathDiv("(a^nx)^{-1}")}
+        {mathDiv("(a^{n+1})^{-1}")}
       </>,
     },
-
-      
-      
-      // x^ax^b = x^{a+b}
-      // (x^a^b) = x^{ab}
-      // x^ay^a = (xy)^a
-
+    {
+      intro: <CheatSheets>
+        <FieldAxiomsCheatSheet />
+        <ExponentiationCheatSheet />
+      </CheatSheets>,
+      problem: <>
+        <p>
+          {letField} Let {mathSpan("a #in F")}, {isNat0("p, q")}. Use induction to prove that
+        </p>
+        {mathDiv("a^pa^q = a^{p+q}")}
+      </>,
+      answer: <>
+        TODO
+      </>,
+    },
+    {
+      intro: <CheatSheets>
+        <FieldAxiomsCheatSheet />
+        <ExponentiationCheatSheet />
+      </CheatSheets>,
+      problem: <>
+        <p>
+          {letField} Let {mathSpan("a #in F")}, {isNat0("p, q")}. Use induction to prove that
+        </p>
+        {mathDiv("(a^p)^q = a^{pq}")}
+      </>,
+      answer: <>
+        TODO
+      </>,
+    },
+    {
+      intro: <CheatSheets>
+        <FieldAxiomsCheatSheet />
+        <ExponentiationCheatSheet />
+      </CheatSheets>,
+      problem: <>
+        <p>
+          {letField} Let {mathSpan("a #in F")}, {isNat0("p")}. Use induction to prove that
+        </p>
+        {mathDiv("a^pb^p = (ap)^p")}
+      </>,
+      answer: <>
+        TODO
+      </>,
+    },
   ],
 };

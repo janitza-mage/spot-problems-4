7c78a4fd6ea19fec93eca5eb9fa73aa71f50d23d
Martin Geisse <martin.geisse@janitza.de>
2026-01-03 10:55:02 +0100

    ...

--- src/content/math/mathSubtree.tsx
+++ src/content/math/mathSubtree.tsx
@@ -7,18 +7,3 @@ export const mathSubtree: Folder = {
   type: "folder",
   elements: [calculusSubtree],
 };
-
-// TODO SChreibweisen: a^b (b in nat) + rechenregeln, Summe (auch Elemente rein/rausziehen, indexverschiebung, leere summe), Produkt, Fakultät, "q.e.d."
-// TODO Grundlagen: Mengen, Beweisverfahren, wchtige Mengen zb nat, Funktionen, Relationen, Kreuzprodukt/Tupel
-// Quantoren, und/oder/implikation/äquivalenz
-
-/*
-
-5. Let the set A consist of the youths of a given group, and the
-set B o f the girls of the same group. Find A\JB, AO B, A\B . Also,
-consider the case when A or B is an empty set.
-"find" -> "what is the meaning of"
-
-
-
- */

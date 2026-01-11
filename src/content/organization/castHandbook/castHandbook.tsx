c18fdcb816882381fe6c9f65e00708cc8ce35b9f
Martin Geisse <martin.geisse@janitza.de>
2026-01-11 11:01:09 +0100

    ...

--- src/content/organization/castHandbook/castHandbook.tsx
+++ src/content/organization/castHandbook/castHandbook.tsx
@@ -1,9 +1,22 @@
-import type {Folder} from "../../../framework/content.tsx";
+import type {Collection} from "../../../framework/content.tsx";
 
-export const castHandbook: Folder = {
+export const castHandbook: Collection = {
   id: "castHandbook",
-  name: "Book: CAST Handbook", // TODO
-  type: "folder",
-  elements: [
+  name: "Book: CAST Handbook",
+  type: "collection",
+  exercises: [
+    {
+      type: "exercise",
+      label: "CAST and STPA",
+      problem: <>
+        <p>
+          What is the similarioty and the difference between CAST and STPA?
+        </p>
+      </>,
+      answer: <p>
+        Both are meant to prevent future accidents. STPA is used <i>before</i> an accident occurs, and tries to predict
+        it. CAST is used <i>after</i> an accident or near-miss occurs, to learn as much from it as possible.
+      </p>,
+    },
   ],
 };

3629a5add8e599b2e2b92df966293f2b51d9e900
Martin Geisse <martin.geisse@janitza.de>
2026-03-03 11:07:23 +0100

    ...

--- src/framework/pages/content/FolderPage.tsx
+++ src/framework/pages/content/FolderPage.tsx
@@ -1,5 +1,5 @@
-import {checkForDuplicateChildIds, type FolderNode} from "../../content.tsx";
-import {FolderPageBreadcrumbs} from "./FolderPageBreadcrumbs";
+import {checkForDuplicateChildIds, type Folder} from "../../content.tsx";
+import {NavigationPageBreadcrumbs} from "./NavigationPageBreadcrumbs.tsx";
 import {NavigationList} from "../../technical-components/navigation/NavigationList/NavigationList.tsx";
 import {
   useNavigateToContentNode
@@ -8,23 +8,23 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import {WithHeader} from "../../technical-components/layout/WithHeader.tsx";
 
 export interface FolderPageProps {
-    folder: FolderNode;
+    folder: Folder;
     path: string[];
 }
 
 export function FolderPage(props: FolderPageProps) {
   checkForDuplicateChildIds(props.folder);
     const navigateToContentNode = useNavigateToContentNode();
-    const navigationElements = props.folder.children.map(child => {
+    const navigationElements = props.folder.elements.map(element => {
         return {
-            label: child.name,
-            decoration: child.type === "folder" ? <ChevronRightIcon /> : <></>,
-            onClick: () => navigateToContentNode([...props.path, child.id]),
+            label: element.name,
+            decoration: element.type === "folder" ? <ChevronRightIcon /> : <></>,
+            onClick: () => navigateToContentNode([...props.path, element.id]),
         };
     });
     return <WithHeader
         header={<div style={{backgroundColor: "#ccc", borderBottom: "1px solid #aaa"}}>
-            <FolderPageBreadcrumbs path={props.path} />
+            <NavigationPageBreadcrumbs path={props.path} />
             <h1 style={{margin: 0}}>{props.folder.name}</h1>
         </div>}
         overflow={"hidden scroll"}

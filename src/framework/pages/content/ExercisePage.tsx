8b2203b8e89be35e8af74c72fcb077b6611d539a
Martin Geisse <martin.geisse@janitza.de>
2026-03-03 11:09:54 +0100

    ...

--- src/framework/pages/content/ExercisePage.tsx
+++ src/framework/pages/content/ExercisePage.tsx
@@ -1,105 +1,105 @@
-import {type ContentItemId, type ExerciseNode} from "../../content.tsx";
-import {useEffect, useState} from "react";
+import {type Exercise} from "../../content.tsx";
+import {useState} from "react";
 import {
   useNavigateToContentNode
 } from "../../technical-components/navigation/ContentNodeLink/useNavigateToContentNode.ts";
-import {type AnswerQuality, selectContentItem, updateState} from "../../../state.tsx";
 import {PageWithHeader} from "../../technical-components/layout/PageWithHeader.tsx";
-import {Button, Dialog, IconButton} from "@mui/material";
+import {Button, IconButton} from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
-import {ContentItemView} from "./ContentItemView.tsx";
-import {ExerciseControlModal} from "./ExerciseControlModal.tsx";
+import LeftIcon from "@mui/icons-material/ChevronLeft";
+import RightIcon from "@mui/icons-material/ChevronRight";
+import {getExerciseLabel} from "../../getExerciseLabel.tsx";
+import {getContentNodeByPath} from "../../paths.tsx";
+import {useGlobalHotkeyListener} from "../../technical-components/util/useGlobalKeyEventListener.tsx";
 
 export interface ExercisePageProps {
   path: string[];
-  exerciseNode: ExerciseNode;
-}
-
-function getNewContentItemId(props: ExercisePageProps): ContentItemId | null {
-  const result = selectContentItem(props.path, props.exerciseNode.contentItems.length);
-  if (result === null) {
-    return null;
-  }
-  return result;
+  exercise: Exercise;
 }
 
 export function ExercisePage(props: ExercisePageProps) {
   const navigateToContentNode = useNavigateToContentNode();
+  const exerciseIndex = Number.parseInt(props.path[props.path.length - 1], 10);
   
-  const [instanceCounter, setInstanceCounter] = useState(0);
-  const [contentItemId, setContentItemId] = useState<ContentItemId | null>(() => getNewContentItemId(props));
-  const [exerciseControlModalOpen, setExerciseControlModalOpen] = useState(false);
+  const [revealed, setRevealed] = useState(false);
 
-  function onClickGrade(answerQuality: AnswerQuality) {
-    if (contentItemId !== null) {
-      updateState(props.path, contentItemId, answerQuality);
-      setInstanceCounter(instanceCounter + 1);
-      setContentItemId(getNewContentItemId(props));
+  function onClickReveal() {
+    setRevealed(true);
+  }
+
+  function onClickPrevious() {
+    if (exerciseIndex > 0) {
+      const path = [...props.path];
+      path.pop();
+      path.push(String(exerciseIndex - 1));
+      navigateToContentNode(path);
     }
   }
 
-  function leaveExercise() {
-    const parentPath = [...props.path];
-    parentPath.pop();
-    navigateToContentNode(parentPath);
-  }
-  
   function onClickCancel() {
-    // eslint-disable-next-line no-restricted-globals
-    if (contentItemId === null || confirm("Really leave?")) {
-      leaveExercise();
-    }
+    const path = [...props.path];
+    path.pop();
+    navigateToContentNode(path);
   }
 
-  // Remove stale state when the content tree changes. The code below handles the stale state case so it won't crash
-  // the application, but we still have to remove that state here to avoid a softlock.
-  useEffect(() => {
-    if (contentItemId !== null && !props.exerciseNode.contentItems[contentItemId]) {
-      onClickGrade("remove");
+  function onClickNext() {
+    const path = [...props.path];
+    path.pop();
+    path.push(String(exerciseIndex + 1));
+    if (getContentNodeByPath(path) !== null) {
+      navigateToContentNode(path);
     }
-  }, [props.exerciseNode, props.exerciseNode.contentItems, contentItemId]);
-
-  // add ctrl+alt+p shortcut to skip content items during development
-  useEffect(() => {
-    const keyHandler = (event: KeyboardEvent) => {
-      if (contentItemId !== null && event.ctrlKey && event.altKey && event.key.toLowerCase() === "p") {
-        onClickGrade(3);
-      }
-    };
-    window.addEventListener("keydown", keyHandler, { capture: true });
-    return () => {
-      window.removeEventListener("keydown", keyHandler, { capture: true });
-    };
-  });
+  }
   
+  useGlobalHotkeyListener(event => {
+    switch (event.key) {
+
+      case "ArrowLeft":
+        onClickPrevious();
+        break;
+
+      case "ArrowRight":
+        onClickNext();
+        break;
+
+      case "ArrowUp":
+      case "Escape":
+        onClickCancel();
+        break;
+
+    }
+  });
+
   return <>
-    <Dialog open={exerciseControlModalOpen} onClose={() => setExerciseControlModalOpen(false)} fullWidth={true}>
-      <ExerciseControlModal path={props.path} exerciseNode={props.exerciseNode} />
-    </Dialog>
     <PageWithHeader
         header={<>
-          <div style={{float: "right"}}>
-            <Button onClick={() => setExerciseControlModalOpen(true)}>C</Button>
-          </div>
-          <h1 style={{margin: 0}}>
-            <IconButton onClick={onClickCancel} sx={{marginRight: "1em"}}>
+          <h1 style={{margin: 0, fontSize: "1em"}}>
+            <IconButton onClick={onClickPrevious}>
+              <LeftIcon fontSize={"large"} />
+            </IconButton>
+            <IconButton onClick={onClickCancel}>
               <CancelIcon fontSize={"large"} />
             </IconButton>
-            {props.exerciseNode.name}
+            <IconButton onClick={onClickNext} sx={{marginRight: "1em"}}>
+              <RightIcon fontSize={"large"} />
+            </IconButton>
+            {getExerciseLabel(props.exercise, exerciseIndex)}
           </h1>
         </>}
     >
-      {contentItemId === null && <>
-          <p>
-              You finished this topic for now. Come back after some time to repeat what you have learned.
-          </p>
-          <p style={{textAlign: "center"}}>
-              <Button onClick={leaveExercise} variant={"contained"}>Leave Exercise</Button>
-          </p>
-      </>}
-      {contentItemId !== null && <>
-          <ContentItemView key={instanceCounter} contentItem={props.exerciseNode.contentItems[contentItemId]} onClickGrade={onClickGrade} />
-      </>}
+      <div style={{margin: "0.2em"}}>
+        <div>{props.exercise.intro}</div>
+        <h2>Problem</h2>
+        <div>{props.exercise.problem}</div>
+        {!revealed && <div style={{textAlign: "center"}}>
+            <Button variant={"contained"} onClick={onClickReveal}>Show Answer</Button>
+        </div>}
+        {revealed && <>
+            <h2>Solution</h2>
+            <div>{props.exercise.answer}</div>
+        </>}
+        <br />
+      </div>
     </PageWithHeader>
   </>;
 }

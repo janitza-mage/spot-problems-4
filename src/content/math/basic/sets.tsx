5a3d92fd460d64bd69ecbf0ee5665e4830f68676
Martin Geisse <martin.geisse@janitza.de>
2026-03-13 11:59:56 +0100

    ...

--- src/content/math/basic/sets.tsx
+++ src/content/math/basic/sets.tsx
@@ -173,5 +173,31 @@ export const sets: Collection = {
         </p>
       </>,
     },
+    {
+      type: "exercise",
+      label: <>Cartesian Product</>,
+      problem: <>
+        <p>Determine the following</p>
+        {mathDiv("#{1, 2, 3#} #times #{100, 200#}")}
+        {mathDiv("#{1, 2, 3#} #times #{1, 2#}")}
+        {mathDiv("#{1#} #times #{1#}")}
+        {mathDiv("#{1#} #times #{#}")}
+        {mathDiv("#{#} #times #{#}")}
+      </>,
+      answer: <>
+        {mathDiv("#{1, 2, 3#} #times #{100, 200#} = ## #{(1, 100), (1, 200), (2, 100), (2, 200), (3, 100), (3, 200)#}")}
+        {mathDiv("#{1, 2, 3#} #times #{1, 2#} = ## #{(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)#}")}
+        {mathDiv("#{1#} #times #{1#} = #{(1, 1)#}")}
+        {mathDiv("#{1#} #times #{#} = #{#}")}
+        {mathDiv("#{#} #times #{#} = #{#}")}
+      </>,
+    },
   ],
 };
+
+/*
+TODO sets
+ 
+    infinite
+
+ */

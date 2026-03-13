abc345c5fc8570220b7d4e0131306b94eca48dcd
Martin Geisse <martin.geisse@janitza.de>
2026-03-13 11:44:28 +0100

    ...

--- src/content/math/basic/sets.tsx
+++ src/content/math/basic/sets.tsx
@@ -99,17 +99,79 @@ export const sets: Collection = {
         {mathDiv("#{x #in #mathbb{N}^+_0 | x < 10 #land x #text{ is even }#}")}
       </>,
       answer: <>
+        {mathDiv("#{0, 2, 4, 6, 8#}")}
+      </>,
+    },
+    {
+      type: "exercise",
+      label: <>Set Comprehension</>,
+      problem: <>
+        <p>Let</p>
+        {mathDiv("A := #{x #in #mathbb{N}^+_0 | x < 10 #land x #text{ is even }#}")}
+        <p>Determine the elements of the following sets.</p>
+        {mathDiv("#{x #in A | x #text{ is a multiple of } 3#}")}
+      </>,
+      answer: <>
+        {mathDiv("#{0, 6#}")}
+      </>,
+    },
+    {
+      type: "exercise",
+      label: <>Set Operators</>,
+      problem: <>
+        <p>Let</p>
+        {mathDiv("A := #{1, 2, 3, 4, 5#}")}
+        {mathDiv("B := #{3, 4, 5, 6, 7#}")}
+        <p>Determine the elements of the following sets.</p>
+        {mathDiv("A #cup B")}
+        {mathDiv("A #cap B")}
+        {mathDiv("A #setminus B")}
+      </>,
+      answer: <>
+        {mathDiv("A #cup B = #{1, 2, 3, 4, 5, 6, 7#}")}
+        {mathDiv("A #cap B = #{3, 4, 5#}")}
+        {mathDiv("A #setminus B = #{1, 2#}")}
+      </>,
+    },
+    {
+      type: "exercise",
+      label: <>The Meaning of "and"</>,
+      problem: <>
+        <p>Let</p>
+        {mathDiv("E := #{x #in #mathbb{N}^+_0 | x #text{ is even }#}")}
+        {mathDiv("O := #{x #in #mathbb{N}^+_0 | x #text{ is odd }#}")}
+        <p>
+          be the sets of even numbers and odd numbers.
+        </p>
+        <p>
+          Next, the set X shall be a set that contains the even numbers <i>and</i> the odd numbers. The symbol for
+          logical "and" is {mathSpan("#land")}, so you might write:
+        </p>
+        {mathDiv("X := #{x #in #mathbb{N}^+_0 | x #text{ is even } #land x #text{ is odd }#}")}
+        <p>
+          However, this is not correct. Why?
+        </p>
+      </>,
+      answer: <>
+        <p>
+          The meaning of the set X as defined above is "the set of all numbers that are even <i>and</i> odd, at the
+          same time". It is the empty set because there are no such numbers.
+        </p>
+        <p>
+          The problem here is a double meaning of the word "and". When applied to a logical condition, it means that
+          both conditions must hold at the same time. When applied to sets, however, it means the union of those sets:
+        </p>
+        {mathDiv("X := E #cup O")}
+        <p>
+          Since {mathSpan("E")} and {mathSpan("O")} are defined using logical conditions, {mathSpan("X")} is the set
+          of numbers where <i>any</i> of those conditions holds, that is, a logical <i>or</i>:
+        </p>
+        {mathDiv("X := #{x #in #mathbb{N}^+_0 | x #text{ is even } #lor x #text{ is odd }#}")}
+        <p>
+          This double meaning of the word "and" must be kept in mind to interpret that word correctly when it appears
+          in a problem.
+        </p>
       </>,
     },
   ],
 };
-
-// circle the best description of union(A, B): "C and D" (???), C but not D, D but not C, C or D or both
-// same for intersection
-// "and" logical operator vs "and" which could mean union or intersection
-// A = 1, 2, 3, 4, 5
-// B = 3, 4, 5, 6, 7
-// union(A, B) = 1, 2, 3, 4, 5, 6, 7
-// intersection(A, B) = 3, 4, 5
-// same with \
-
